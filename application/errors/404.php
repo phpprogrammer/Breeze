345b80c8b0f4f23886b31b19afc6599a3554bebc
tsapeta <tomasz.sapeta@me.com>
2013-03-21 10:57:23 +0100

    visual fixes in 404 and custom error file

--- application/errors/404.php
+++ application/errors/404.php
@@ -14,7 +14,8 @@ div#container {
 }
 section { 
 	display:inline-block;
-	margin:0 auto 40px; 
+	width:33%;
+    margin:0 auto 40px; 
 	padding:30px 100px;
 	background:#ededed; 
 	border:1px solid #ccc; 
@@ -22,9 +23,9 @@ section {
 	box-shadow:inset 0 1px 0 white,inset 0 -1px 0 white,inset 1px 0 0 white,inset -1px 0 0 white, 0 0 10px rgba(0,0,0,0.2); 
 }
 h1 { font-size:28px; }
-h3 { font-size:24px; }
+h2 { font-size:24px; }
 p { font-size:20px; }
-p, h1, h3 { margin:25px 0; }
+p, h1, h2 { margin:25px 0; }
 p#url { color:#E13300; font-size:20px; font-weight:bold; font-style:italic; }
 </style>
 
@@ -32,7 +33,7 @@ p#url { color:#E13300; font-size:20px; font-weight:bold; font-style:italic; }
 	<div id="container">
 		<section>
 			<h1>HTTP 404 Error</h1>
-			<h3>File or directory not found</h3>
+			<h2>File or directory not found</h2>
 			<p id="url"><?='http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']?></p>
 			<p>Oops! We couldn't find this page for you.</p>
 		</section>
