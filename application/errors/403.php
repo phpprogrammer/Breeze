82040d8c0c79a0e64857ed1a418bccea9b40bb24
tsapeta <tomasz.sapeta@me.com>
2013-03-21 10:55:52 +0100

    rename error file access_denied to 403

--- application/errors/403.php
+++ application/errors/403.php
@@ -1,7 +1,7 @@
 <!doctype html>
 <html>
 <head>
-	<title>404 Page Not Found</title>
+	<title>403 Permission Denied</title>
 </head>
 
 <style type="text/css">
@@ -14,6 +14,7 @@ div#container {
 }
 section { 
 	display:inline-block;
+    width:33%;
 	margin:0 auto 40px; 
 	padding:30px 100px;
 	background:#ededed; 
@@ -22,16 +23,17 @@ section {
 	box-shadow:inset 0 1px 0 white,inset 0 -1px 0 white,inset 1px 0 0 white,inset -1px 0 0 white, 0 0 10px rgba(0,0,0,0.2); 
 }
 h1 { font-size:28px; }
-h3 { font-size:24px; }
+h2 { font-size:24px; }
 p { font-size:20px; }
-p, h1, h3 { margin:25px 0; }
+p, h1, h2 { margin:25px 0; }
 p#url { color:#E13300; font-size:20px; font-weight:bold; font-style:italic; }
 </style>
 
 <body>
 	<div id="container">
 		<section>
-			<h1>Access Denied</h1>
+            <h1>HTTP 403 Error</h1>
+			<h2>Access Denied</h2>
 			<p id="url"><?='http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']?></p>
 			<p>We're sorry, but you aren't allowed to view this page!</p>
 		</section>
