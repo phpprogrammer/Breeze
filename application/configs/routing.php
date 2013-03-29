ecc26e7de5ca1fb05b644282cae39ab3adac30f9
tsapeta <tomasz.sapeta@me.com>
2013-03-29 16:03:21 +0100

    new reject_ajax_request memory variable

--- application/configs/routing.php
+++ application/configs/routing.php
@@ -1,7 +1,9 @@
 <?php
-	$allowed_extensions = array (  0 => 'css',  1 => 'js',  2 => 'less',  3 => 'jpg',  4 => 'png',  5 => 'gif',);
-	$default_action = 'index';
+    
+    $splitter = "/";
+    $allowed_extensions = array('css','js','less','jpg','png','gif');
+    $default_action = 'index';
 	$default_controller = 'home';
-	$default_language = 'pl';
-	$redirect_to_default = true;
-	$splitter = '/';
+    $default_language = 'pl';
+    $redirect_to_default = true;
+    $reject_ajax_requests = false;
