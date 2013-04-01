4f97a4aece92affe14e5a1d37114151a9fa8c36c
tsapeta <tomasz.sapeta@me.com>
2013-04-01 17:06:32 +0200

    routing memory update

--- application/configs/routing.php
+++ application/configs/routing.php
@@ -1,9 +1,8 @@
 <?php
-    
-    $splitter = "/";
-    $allowed_extensions = array('css','js','less','jpg','png','gif');
-    $default_action = 'index';
+	$allowed_extensions = array (  0 => 'css',  1 => 'js',  2 => 'less',  3 => 'jpg',  4 => 'png',  5 => 'gif',);
+	$default_action = 'index';
 	$default_controller = 'home';
-    $default_language = 'pl';
-    $redirect_to_default = true;
-    $reject_ajax_requests = false;
+	$default_language = 'pl';
+	$redirect_to_default = true;
+	$reject_ajax_requests = false;
+	$splitter = '/';
