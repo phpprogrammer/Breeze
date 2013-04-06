4c01217be5d2ee5c7530b3e9e35835c0dbd26ba6
tsapeta <tomasz.sapeta@me.com>
2013-04-06 19:12:34 +0200

    added array_set and array_get methods
    They add support for multi-dimensional arrays through simple key with
    dots (like JavaScript)

--- system/helpers/main_functions.php
+++ system/helpers/main_functions.php
@@ -27,6 +27,69 @@
         \system\core\Application::$view->import($viewName);
     }
     
+    function array_set(Array &$array, $key, $value)
+    {
+        $keys = explode('.', $key);
+        $s =& $array;
+        
+        while (count($keys) > 1) {
+            $k = array_shift($keys);
+            if (! isset($s[$k]) || ! is_array($s[$k])) {
+                $s[$k] = array();
+            }
+            $s =& $s[$k];
+        }
+        $k = reset($keys);
+        if ($value === '++' || $value === '--') {
+            if (! isset($s[$k]) || ! is_integer($s[$k])) {
+                $s[$k] = 0;
+            }
+            switch ($value) {
+                case '++':
+                    $value = $s[$k] + 1;
+                    break;
+                case '--':
+                    $value = $s[$k] - 1;
+                    break;
+            }
+        }
+        if (!isset($s[$k]) || $s[$k] !== $value) {
+            if ($value === null) {
+                unset($s[$k]);
+            } else {
+                $s[$k] = $value;
+            }
+            return true;
+        } else {
+            return false;
+        }
+    }
+    
+    function array_get(Array &$array, $arg1)
+    {
+        if (strpos($arg1, '.')) {
+            $args = explode('.', $arg1);
+        } else {
+            $args = func_get_args();
+            array_shift($args);
+        }
+        $s =& $array;
+        while (count($args) > 1) {
+            $key = array_shift($args);
+            if (isset($s[$key])) {
+                $s =& $s[$key];
+            } else {
+                return null;
+            }
+        }
+        $key = reset($args);
+        if (!empty($s[$key])) {
+            return $s[$key];
+        } else {
+            return null;
+        }
+    }
+    
     if (! function_exists("array_column")) {
         function array_column($array, $column)
         {
