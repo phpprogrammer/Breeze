fab722ad1398d44d4a4e674da4e5304410683a77
tsapeta <tomasz.sapeta@me.com>
2013-04-06 22:41:39 +0200

    insignificant fix (code formatting)

--- index.php
+++ index.php
@@ -1,20 +1,20 @@
 <?php	
-	define('DS', DIRECTORY_SEPARATOR);
-	define('ROOT', rtrim(dirname(__FILE__), DS) . DS);
-	require(ROOT . 'system/defaults/paths.php');
-		
-	function __autoload($path) {
-		$path = ROOT . str_replace('\\', DS, $path) . '.php';
-		if(is_readable($path)) {
-			require($path);
-		}
-	}
+    define('DS', DIRECTORY_SEPARATOR);
+    define('ROOT', rtrim(dirname(__FILE__), DS) . DS);
+    require(ROOT . 'system/defaults/paths.php');
+    
+    function __autoload($path) {
+        $path = ROOT . str_replace('\\', DS, $path) . '.php';
+        if(is_readable($path)) {
+            require($path);
+        }
+    }
     use \system\core\String;
-	define('DEFAULT_PATH', String::path_wrap(String::rbreak($_SERVER['SCRIPT_NAME'], 'index.php')));
-
-	// 'development' || 'testing' || 'production'
-	define('ENVIRONMENT', 'development');
-	
-	// And away we go...
+    define('DEFAULT_PATH', String::path_wrap(String::rbreak($_SERVER['SCRIPT_NAME'], 'index.php')));
+    
+    // 'development' || 'testing' || 'production'
+    define('ENVIRONMENT', 'development');
+    
+    // And away we go...
     
     require(CORE_PATH . 'Bootstrap.php');
